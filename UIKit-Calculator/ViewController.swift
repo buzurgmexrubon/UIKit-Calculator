714f9da0ffaa937e4fd7753683558c2cc654042d
Buzurgmexr Sultonaliyev <sultonaliyevbuzurgmexr@gmial.com>
2023-08-20 13:43:42 +0500

    Initial Commit

---
+++ UIKit-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-Calculator
+//
+//  Created by Buzurgmexr Sultonaliyev on 20/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
