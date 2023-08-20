2c084dfcea4dc06ad58abcd23e7e09a22327a890
Buzurgmexr Sultonaliyev <sultonaliyevbuzurgmexr@gmial.com>
2023-08-20 13:58:24 +0500

    add Token.swift and TokenType.swift files for implementing tokens in calculator

---
+++ UIKit-Calculator/Model/Token/Token.swift
@@ -0,0 +1,10 @@
+//
+//  Token.swift
+//  UIKit-Calculator
+//
+//  Created by Buzurgmexr Sultonaliyev on 20/08/23.
+//
+
+struct Token {
+  var content: TokenType
+}
---
+++ UIKit-Calculator/Model/Token/TokenType.swift
@@ -0,0 +1,12 @@
+//
+//  TokenType.swift
+//  UIKit-Calculator
+//
+//  Created by Buzurgmexr Sultonaliyev on 20/08/23.
+//
+
+enum TokenType {
+  case operand
+  case unaryOperator
+  case binaryOperator
+}
