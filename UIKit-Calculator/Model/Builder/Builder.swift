d759c5b7c3c2501c11f83678d920c4f776a1e738
Buzurgmexr Sultonaliyev <sultonaliyevbuzurgmexr@gmial.com>
2023-08-20 22:57:44 +0500

    - 🥳 add builder section

---
+++ UIKit-Calculator/Model/Builder/Builder.swift
@@ -0,0 +1,52 @@
+//
+//  Builder.swift
+//  UIKit-Calculator
+//
+//  Created by Buzurgmexr Sultonaliyev on 20/08/23.
+//
+
+class Builder<Element> {
+  // MARK: Properties
+
+  /// The array of the `Element` for store values.
+  private var elements: [Element] = []
+
+  /// The array of the `Element` for getting and setting value.
+  var value: [Element] {
+    get { elements }
+    set { elements.append(newValue[0]) }
+  }
+
+  /// Count elements of the `elements`.
+  var count: Int { elements.count }
+
+  // MARK: - Methods
+
+  /// Removes the last element of the `elements`.
+  ///
+  /// - Returns: no return value.
+  @discardableResult
+  func remove() -> Element? {
+    elements.popLast()
+  }
+
+  /// Get last value of the `elements`.
+  ///
+  /// - Returns: The last element of the `elements`.
+  func peak() -> Element? {
+    elements.last
+  }
+
+  /// Build expression.
+  ///
+  /// - Returns: Array of the `Element`.
+  func build() -> [Element] {
+    defer { clear() }
+    return elements
+  }
+
+  /// Remove all elements of the `elements`.
+  func clear() {
+    elements.removeAll()
+  }
+}
